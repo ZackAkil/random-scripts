7243a9b8baf7e41211e72b27511259d024b0600c
Zack <zackakil94@gmail.com>
2016-10-21 10:40:30 +0100

    Update horz.py

--- horz.py
+++ horz.py
@@ -1,9 +1,10 @@
 import math
 
 def horz(height):
+	"""Returns the distance to horizon (in meters) based on persion height"""
 	earthRadius = 6371000
 	horzTheta = math.degrees(math.acos(earthRadius*1.0/(earthRadius+height)))
 	groundDist = 2*math.pi*earthRadius*(horzTheta/360.0)
 	pythDirect = math.sqrt((earthRadius+height)**2 - earthRadius**2)
 	return groundDist,pythDirect,groundDist-pythDirect
-print horz(101.8)
+print horz(1.8)
